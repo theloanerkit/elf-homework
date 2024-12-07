cab98a31df5ff1c912a9e0babce24b0781f4c20d
theloanerkit <126063546+theloanerkit@users.noreply.github.com>
2024-12-07 19:03:37 +0000

    day 6 is faster but not fast enough, and has a load bearing print statement

--- fortran/2024_06.f90
+++ fortran/2024_06.f90
@@ -1,4 +1,4 @@
-program day06
+program guard_gallivant
     use reader
     implicit none
     integer :: file_len, line_len
@@ -16,19 +16,34 @@ program day06
     subroutine run(file_len,line_len)
         integer :: file_len, line_len
         character :: data(file_len,line_len)
-        integer :: ans1, ans2,i
+        integer :: ans1, ans2,i,j,idx
+        integer :: walls(file_len*line_len),c
+        integer,allocatable :: just_walls(:)
         ans1 = 0
         ans2 = 0
         data=grid_reader(file_len,line_len)
         do i=1,file_len
             print*,data(i,:)
         end do
-        print*,"--------"
-        call part1(data,file_len,line_len,ans1,.false.)
-        call part2(data,file_len,line_len,ans2)
-        !do i=1,file_len
-        !    print*,data(i,:)
-        !end do
+        print*," "
+        call part1(data,file_len,line_len,ans1,walls)
+        do i=1,file_len
+            print*,data(i,:)
+        end do
+        c=count(walls.eq.-1)
+        idx=1
+        allocate(just_walls(file_len*line_len-c))
+        do j=1,file_len*line_len
+            if (walls(j).ne.-1) then
+                just_walls(idx)=walls(j)
+                !print*,just_walls(idx)
+                idx=idx+1
+            end if 
+        end do
+        !print*,just_walls
+        !print*,"yo"
+        call part2(data,file_len,line_len,ans2,just_walls,file_len*line_len-c)
+        !print*,c,file_len*line_len
         print*,"part 1 answer: ",ans1
         print*,"part 2 answer: ",ans2
     end subroutine run
@@ -38,16 +53,49 @@ program day06
         ans = (0.5*(x+y)*(x+y+1))+y
     end function pair
 
-    subroutine part1(grid,file_len,line_len,ans1,l)
+    subroutine guard_step(pos,nextpos,dirn,file_len,line_len,grid,visited,visit_idx)
+        integer :: pos(2),dirn(2),file_len,line_len,nextpos(2)
+        integer :: key,key_idx(1),visited(file_len*line_len),visit_idx,col,row
+        character :: grid(file_len,line_len)
+        nextpos(1) = pos(1)+dirn(1)
+        nextpos(2) = pos(2)+dirn(2)
+        if (nextpos(1).eq.0.or.nextpos(1).eq.file_len+1.or.nextpos(2).eq.0.or.nextpos(2).eq.line_len+1) then
+            nextpos=(/-1,-1/)
+        else if (grid(nextpos(1),nextpos(2)).eq.".".or.grid(nextpos(1),nextpos(2)).eq."X") then
+            grid(pos(1),pos(2))="X"
+            key = pair(pos(1),pos(2))
+            key_idx = findloc(visited,key)
+            if (key_idx(1).eq.0) then
+                visited(visit_idx)=key
+                visit_idx=visit_idx+1
+            end if
+        else if (grid(nextpos(1),nextpos(2)).eq."#") then
+            if (dirn(1).eq.1.or.dirn(1).eq.-1) then
+                col = -dirn(1)
+                dirn(1)=0
+                dirn(2)=col
+            else if (dirn(2).eq.1.or.dirn(2).eq.-1) then
+                row = dirn(2)
+                dirn(2) = 0
+                dirn(1) = row
+            end if
+            nextpos=pos
+        end if
+    end subroutine guard_step
+
+    subroutine part1(grid,file_len,line_len,ans1,new_walls)
         integer :: ans1,file_len,line_len
         character :: grid(file_len,line_len)
         integer :: visited(file_len*line_len),vi,temp,temp2(1),loop(file_len*line_len)
         integer :: dirn(2),pos(2),nextpos(2),row,col,i,step,li,up(2),start(2)
-        integer :: corners(10000),prevcorners(40),ci,cidx(1),cidx2(1)
+        integer :: corners(10000),ci,cidx(1),cidx2(1)
+        integer :: new_walls(file_len*line_len),wi,temppos(2),t
         logical :: go,l,die
         corners = 0
         ci=1
-        prevcorners=0
+        new_walls=-1
+        wi = 1
+        !prevcorners=0
         visited=0
         loop=0
         pos=findloc(grid,"^")
@@ -60,137 +108,161 @@ program day06
         li=1
         up=0
         die=.false.
-        !print*,pos
         do while (go)
-            nextpos(1) = pos(1)+dirn(1)
-            nextpos(2) = pos(2)+dirn(2)
-            !print*,pos,nextpos,dirn
-            if (nextpos(1).eq.0.or.nextpos(1).eq.file_len+1.or.nextpos(2).eq.0.or.nextpos(2).eq.line_len+1) then
-                ! off grid, done
+            call guard_step(pos,nextpos,dirn,file_len,line_len,grid,visited,vi)
+            if (nextpos(1).eq.-1) then
                 go=.false.
-            else if (grid(nextpos(1),nextpos(2)).eq.".".or.grid(nextpos(1),nextpos(2)).eq."X") then
-                !print*,"empty space"
-                grid(pos(1),pos(2))="X"
+            else if (nextpos(1).eq.pos(1).and.nextpos(2).eq.pos(2)) then
+            else
                 pos = nextpos
-                temp = pair(pos(1),pos(2))
-                temp2 = findloc(visited,temp)
-                if (temp2(1).eq.0) then
-                    visited(vi)=temp
-                    vi=vi+1
-                    !ans1=ans1+1
-                end if
-            else if (grid(nextpos(1),nextpos(2)).eq."#") then
-                !print*,"wall"
-                !if (l) then
-                !    if (dirn(1).eq.-1.and.dirn(2).eq.0) then
-                !        if (pos(1).eq.up(1).and.pos(2).eq.up(2)) then
-                !            ans1=0
-                !            go=.false.
-                !        else
-                !            up=pos
-                !        end if
-                !    end if
-                !end if
-                if (dirn(1).eq.1.or.dirn(1).eq.-1) then
-                    col = -dirn(1)
-                    dirn(1)=0
-                    dirn(2)=col
-                else if (dirn(2).eq.1.or.dirn(2).eq.-1) then
-                    row = dirn(2)
-                    dirn(2) = 0
-                    dirn(1) = row
-                end if
-                if (l) then
-                    !print*,corners
-                    corners(ci)=pair(pos(1),pos(2))
-                    cidx=findloc(corners,corners(ci))
-                    
-                   ! print*,ci,cidx
-                    !if (ci.ne.cidx(1)) then
-                    !    corners(cidx(1))=-1
-                    !cidx2=findloc(corners,corners(ci))
-                    !print*,ci,cidx,cidx2
-                    if (cidx(1).ne.ci) then
-                        if (mod(ci-cidx(1),4).eq.0) then!.and.mod(ci-cidx2(1),4).eq.0) then
-                            go=.false.
-                            ans1=0
-                            die=.true.
-                        end if
-                    end if
-                    !corners(cidx(1))=corners(ci)
-                    !end if
-                    ci=ci+1
-                end if
-                grid(pos(1),pos(2))="O"
-                !print*,"----",pos
-                !do i=1,file_len
-                !    print*,grid(i,:)
-                !end do
-                !print*," "
-                step=step+1
-                !if (step.eq.39) then
-                !    go=.false.
-                !end if
-                grid(pos(1),pos(2))="."
-                !print*,"done"
-                !print*,pos
-                !pos = nextpos
-                !if (dirn.eq.(/-1,0/)) then
-                !    dirn=(/0,1/)
-                !else if (dirn.eq.(/0,1/)) then
-                !    dirn=(/1,0/)
-                !else if (dirn.eq.(/1,0/)) then
-                !    dirn=(/0,-1/)
-                !else if (dirn.eq.(/0,-1/)) then
-                !    dirn=(/-1,0/)
-                !end if
             end if
-            !if (vi.eq.20) then
-            !    go=.false.
-            !end if
+            print*,pos
+            if (pos(1)-1.ne.0) then
+                temp = pair(pos(1)-1,pos(2))
+                !t = findloc(new_walls,temp)
+                if (all(findloc(new_walls,temp).eq.0)) then
+                    new_walls(wi) = temp
+                    wi = wi+1
+                    !print*,"new wall"
+                    !print*,"adding",temp,pos(1)-1,pos(2)
+                else
+                    !print*,"repeated",pos(1)-1,pos(2)
+                end if
+            end if
+            if (pos(2)-1.ne.0) then
+                temp = pair(pos(1),pos(2)-1)
+                if (all(findloc(new_walls,temp).eq.0)) then
+                    new_walls(wi) = temp
+                    wi = wi+1
+                    !print*,"new wall"
+                    !print*,"adding",temp,pos(1),pos(2)-1
+                else
+                    !print*,"repeated",pos(1),pos(2)-1
+                end if
+            end if
+            if (pos(1)+1.ne.file_len+1) then
+                temp = pair(pos(1)+1,pos(2))
+                if (all(findloc(new_walls,temp).eq.0)) then
+                    new_walls(wi) = temp
+                    wi = wi+1
+                    !print*,"new wall"
+                    !print*,"adding",temp,pos(1)+1,pos(2)
+                else
+                    !print*,"repeated",pos(1)+1,pos(2)
+                end if
+            end if
+            if (pos(2)+1.ne.line_len+1) then
+                temp = pair(pos(1),pos(2)+1)
+                if (all(findloc(new_walls,temp).eq.0)) then
+                    new_walls(wi) = temp
+                    wi = wi+1
+                    !print*,"new wall"
+                    !print*,"adding",temp,pos(1),pos(2)+1
+                else
+                   ! print*,"repeated",pos(1),pos(2)+1
+                end if
+            end if
         end do
         grid(pos(1),pos(2))="X"
         if (.not.l.or..not.die) then
-            !do i=1,file_len
-            !    print*,grid(i,:)
-            !    
-            !end do
-            !print*,""
             ans1=count(grid.eq."X")
         end if
         grid(start(1),start(2))="^"
     end subroutine part1
 
-    subroutine part2(grid,file_len,line_len,ans2)
-        integer :: file_len,line_len,ans2,i,j,pos(2),t,k
+    subroutine in_a_loop(grid,file_len,line_len,ans1)
+        integer :: ans1,file_len,line_len
         character :: grid(file_len,line_len)
+        integer :: visited(file_len*line_len),vi,temp,temp2(1),loop(file_len*line_len)
+        integer :: dirn(2),pos(2),nextpos(2),row,col,i,step,li,up(2),start(2)
+        integer :: corners(10000),ci,cidx(1),cidx2(1)
+        logical :: go,l,die
+        corners = 0
+        ci=1
+        !prevcorners=0
+        visited=0
+        loop=0
         pos=findloc(grid,"^")
-        print*,pos
+        start=findloc(grid,"^")
+        grid(pos(1),pos(2))="."
+        dirn=(/-1,0/)
+        go=.true.
+        vi=1
+        step=1
+        li=1
+        up=0
+        die=.false.
+        ans1=1
+        do while (go)
+            call guard_step(pos,nextpos,dirn,file_len,line_len,grid,visited,vi)
+            if (nextpos(1).eq.-1) then
+                go=.false.
+            else if (nextpos(1).eq.pos(1).and.nextpos(2).eq.pos(2)) then
+                ! direction has updated
+                if (l) then
+                    corners(ci)=pair(pos(1),pos(2))
+                    cidx=findloc(corners,corners(ci))
+                    if (cidx(1).ne.ci) then
+                        if (mod(ci-cidx(1),4).eq.0) then
+                            go=.false.
+                            ans1=0
+                            die=.true.
+                        end if
+                    end if
+                    ci=ci+1
+                end if
+                step=step+1
+            else
+                pos = nextpos
+            end if
+        end do
+        grid(pos(1),pos(2))="X"
+        grid(start(1),start(2))="^"
+    end subroutine in_a_loop
+
+    subroutine part2(grid,file_len,line_len,ans2,walls,w)
+        integer :: file_len,line_len,ans2,i,j,pos(2),t,k(1),w
+        integer :: walls(w)
+        character :: grid(file_len,line_len)
+        !print*,"hi",w
+        pos=findloc(grid,"^")
+        print*,walls ! load bearing print statement
         do i=1,file_len
             print*,i,"/",file_len
             do j=1,line_len
-                if (i.ne.pos(1).or.j.ne.pos(2)) then
+                !print*," ",j
+                k=findloc(walls,pair(i,j))
+                !print*,pair(i,j),k,i,j
+                if ((i.ne.pos(1).or.j.ne.pos(2)).and.k(1).ne.0) then
+                    !print*,"yo",i,j
                     if (grid(i,j).ne."#") then
-                        !print*,i,j
                         grid(i,j)="#"
-                        !do k=1,file_len
-                        !    print*,grid(k,:)
-                        !end do
-                        !print*,"-----"
-                        call part1(grid,file_len,line_len,t,.true.)
-                        !if (i.eq.7.and.j.eq.4) then
-                        !    print*,t
-                        !end if
+                        !print*,"checking guard"
+                        t=0
+                        call in_a_loop(grid,file_len,line_len,t)
+                        !print*,"hey",t
+                        !print*,t
                         grid(i,j)="."
+                        !print*,i,j
+                        !print*,t
                         if (t.eq.0) then
-                            !print*,"obstacle",i,j
+                            !print*,i,j
                             ans2=ans2+1
+                            !print*,i,j
+                            !print*,"hello"
                         end if
                     end if
                 end if
             end do
         end do
     end subroutine part2
+
+    ! 7 4
+    ! 8 7
+    ! 8 8
+    ! 9 2
+    ! 9 4
+    ! 10 8
 
     subroutine part3(grid,file_len,line_len,ans2)
         integer :: file_len,line_len,ans2,col,row
@@ -390,4 +462,4 @@ program day06
            ! print*,idx
         end do
     end subroutine part3
-end program day06
+end program guard_gallivant
