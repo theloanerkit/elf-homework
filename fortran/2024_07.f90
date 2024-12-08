ed363894f637c62c22a089e0d62c489e2c59ec46
theloanerkit <126063546+theloanerkit@users.noreply.github.com>
2024-12-08 16:13:59 +0000

    now runs in a less painful amount of time, still not great tho, added more conditions for leaving recursion

--- fortran/2024_07.f90
+++ fortran/2024_07.f90
@@ -47,62 +47,97 @@ program day07
             answers(:s+1)=temp
             deallocate(temp)
         end if
-        !print*,size(answers)!,answers
     end subroutine new_answer
 
-    recursive function operators(eqn,l,answers) result (new_eqn)
-        integer(kind=k12) :: l,eqn(l),new_eqn(l),temp(l-1),i
+    subroutine build_next_eqn(temp,eqn,l,op)
+        integer :: op,i
+        integer(kind=k12) :: l
+        integer(kind=k12) :: temp(l-1),eqn(l)
+        if (op.eq.1) then
+            temp(1) = eqn(1)+eqn(2)
+        else if (op.eq.2) then
+            temp(1) = eqn(1)*eqn(2)
+        else if (op.eq.3) then
+            temp(1) = eqn(1)*10**(floor(log10(real(eqn(2))))+1)+eqn(2)
+        end if
+        do i=2,l-1
+            temp(i)=eqn(i+1)
+        end do
+    end subroutine build_next_eqn
+
+    recursive function operators(eqn,l,answers,goal) result (new_eqn)
+        integer(kind=k12) :: l,eqn(l),new_eqn(l),temp(l-1),broke(1)
         integer(kind=k12),allocatable :: answers(:)
-        if (l.eq.1) then
+        integer(kind=k12) :: goal
+        if (eqn(1).gt.goal.and.l.ne.1) then
+            broke(1)=eqn(1)
+            new_eqn=operators(broke,1_k12,answers,goal)
+        else if (l.eq.1) then
             new_eqn(1) = eqn(1)
             call new_answer(answers,eqn(1))
         else
-            temp(1) = eqn(1)+eqn(2)
-            do i=2,l-1
-                temp(i)=eqn(i+1)
-            end do
-            new_eqn=operators(temp,l-1,answers)
-
-            temp(1) = eqn(1)*eqn(2)
-            do i=2,l-1
-                temp(i)=eqn(i+1)
-            end do
-            new_eqn=operators(temp,l-1,answers)
+            if (allocated(answers)) then
+                if (.not.any(answers.eq.goal)) then
+                    call build_next_eqn(temp,eqn,l,1)
+                    new_eqn=operators(temp,l-1,answers,goal)
+                
+                    call build_next_eqn(temp,eqn,l,2)
+                    new_eqn=operators(temp,l-1,answers,goal)
+                end if
+            end if
+            if (.not.allocated(answers)) then
+                call build_next_eqn(temp,eqn,l,1)
+                new_eqn=operators(temp,l-1,answers,goal)
+            
+                call build_next_eqn(temp,eqn,l,2)                
+                new_eqn=operators(temp,l-1,answers,goal)
+            end if
         end if
     end function operators
 
-    recursive function operators2(eqn,l,answers) result (new_eqn)
-        integer(kind=k12) :: l,eqn(l),new_eqn(l),temp(l-1),i,f
+    recursive function operators2(eqn,l,answers,goal) result (new_eqn)
+        integer(kind=k12) :: l,eqn(l),new_eqn(l),temp(l-1),broke(1),goal
         integer(kind=k12),allocatable :: answers(:)
-        real(kind=dp) :: r,pow
-        if (l.eq.1) then
+        !print*,"equation",eqn
+        if (eqn(1).gt.goal.and.l.ne.1) then
+            !print*,"oops too far",eqn(1),goal
+            broke(1)=eqn(1)
+            new_eqn=operators(broke,1_k12,answers,goal)
+        else if (l.eq.1) then
+            !print*,"new answer",eqn(1)
             new_eqn(1) = eqn(1)
             call new_answer(answers,eqn(1))
         else
-            temp(1) = eqn(1)+eqn(2)
-            do i=2,l-1
-                temp(i)=eqn(i+1)
-            end do
-            !print*,temp
-            new_eqn=operators2(temp,l-1,answers)
+            if (allocated(answers)) then
+                if (.not.any(answers.eq.goal)) then
+                    !print*,"no correct answers yet"
+                    call build_next_eqn(temp,eqn,l,1)
+                    !print*,"added",temp
+                    new_eqn=operators2(temp,l-1,answers,goal)
 
-            temp(1) = eqn(1)*eqn(2)
-            do i=2,l-1
-                temp(i)=eqn(i+1)
-            end do
-            !print*,temp
-            new_eqn=operators2(temp,l-1,answers)
+                    call build_next_eqn(temp,eqn,l,2)
+                    !print*,"multiplied",temp
+                    new_eqn=operators2(temp,l-1,answers,goal)
 
-            r=eqn(2)
-            pow=log10(r)
-            f=floor(pow)+1
-            temp(1) = eqn(1)*10**f+eqn(2)
-            do i=2,l-1
-                temp(i)=eqn(i+1)
-            end do
-            !print*,temp
-            new_eqn=operators2(temp,l-1,answers)
-            !print*,"---"
+                    call build_next_eqn(temp,eqn,l,3)
+                    !print*,"concatenated",temp
+                    new_eqn=operators2(temp,l-1,answers,goal)
+                end if
+            end if
+            if (.not.allocated(answers)) then
+                !print*,"no answers yet"
+                call build_next_eqn(temp,eqn,l,1)
+                !print*,"added",temp
+                new_eqn=operators2(temp,l-1,answers,goal)
+
+                call build_next_eqn(temp,eqn,l,2)
+                !print*,"multiplied",temp
+                new_eqn=operators2(temp,l-1,answers,goal)
+
+                call build_next_eqn(temp,eqn,l,3)
+                !print*,"concatenated",temp
+                new_eqn=operators2(temp,l-1,answers,goal)
+            end if
         end if
     end function operators2
 
@@ -116,9 +151,9 @@ program day07
         eqn_num=ints_from_str(nums,eqn,line_len)
         nums16=nums
         if (part.eq.1) then
-            ans = operators(eqn_num(2:),nums16-1,answers)  
+            ans = operators(eqn_num(2:),nums16-1,answers,eqn_num(1))  
         else
-            ans = operators2(eqn_num(2:),nums16-1,answers)  
+            ans = operators2(eqn_num(2:),nums16-1,answers,eqn_num(1))  
         end if
         !print*,answers
         if (any(answers.eq.eqn_num(1))) then
@@ -144,13 +179,15 @@ program day07
     end subroutine part1
 
     subroutine part2(equations,file_len,line_len,ans2)
-        integer(kind=k12) :: ans2,t(1)
+        integer(kind=k12) :: ans2,t(1),g
         integer :: file_len,line_len,i,c
         character(line_len) :: equations(file_len)
         logical :: test
+        g=1
         do i=1,file_len
             print*,i,"/",file_len
             c=count_chars(line_len,equations(i),(/"`"/),1)+1
+            !print*,equations(i)
             test=eqn_valid(line_len,equations(i),c,2)
             if (test) then
                 t = ints_from_str(1,equations(i),line_len)
